f2113273da2fcf5f48d6c7acddbbb6a1d1295d58
Vicki Taylor <vtaylor85@live.com>
2022-09-06 15:21:54 -0500

    components setup

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,34 @@
+import { Link, useNavigate } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = () => {
+    const navigate = useNavigate()
+    return (
+        <ul className="navbar">
+            <Link className="navbar__link" to="/Rooms">
+                <li className="navbar__item">
+                    Rooms
+                </li>
+            </Link>
+
+            {
+                (localStorage.getItem("om_token") !== null) ?
+                    <li className="nav-item">
+                        <button className="nav-link fakeLink"
+                            onClick={() => {
+                                localStorage.removeItem("om_token")
+                                navigate('/login')
+                            }}
+                        >Logout</button>
+                    </li> :
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
+                        </li>
+                    </>
+            }        </ul>
+    )
+}
